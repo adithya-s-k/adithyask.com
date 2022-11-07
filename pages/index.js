666e37caf3eab8ff5874e81671c04367e681de94
Adithya S K <adithyaskolavi@gmail.com>
2022-11-08 00:25:36 +0530

    layout component added - Adithya S K

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Home</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde non vero voluptas laborum et asperiores sunt soluta aut eligendi, provident culpa quam tempora labore, quasi error voluptatibus consectetur incidunt eius.</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde non vero voluptas laborum et asperiores sunt soluta aut eligendi, provident culpa quam tempora labore, quasi error voluptatibus consectetur incidunt eius.</p>
 
       <Link href="items">See Items</Link>
-      <Footer/>
     </div>
   )
 }
