62c9a17773c80677a47ea349929ee8ccc8c8780f
Adithya S K <adithyaskolavi@gmail.com>
2023-12-05 01:50:48 +0530

    added footer - Adithya S K

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import HeroSection from './sections/Hero';
 import Aboutme from './sections/Aboutme';
+import Footer from './sections/Footer';
 
 export default function Home() {
   return (
     <>
       <div className="h-fit">
-        <div className="h-screen w-full">
+        <div className="min-h-screen h-fit w-full">
           <HeroSection />
         </div>
-        <div className="h-screen w-full">
+        <div className="min-h-screen h-fit w-full">
           <Aboutme />
+        </div>
+        <div className="h-fit w-full">
+          <Footer />
         </div>
       </div>
     </>
