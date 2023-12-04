e1a1f485868a8fad9ba9e9449f14d2635868c980
Adithya S K <adithyaskolavi@gmail.com>
2023-12-05 01:51:24 +0530

    made green card component - Adithya SK

--- app/components/neongreencolor.tsx
+++ app/components/neongreencolor.tsx
@@ -8,7 +8,7 @@ import {
 
 import { MouseEventHandler, PropsWithChildren } from 'react';
 
-export const Card: React.FC<PropsWithChildren> = ({ children }) => {
+export const GreenColor: React.FC<PropsWithChildren> = ({ children }) => {
   const mouseX = useSpring(0, { stiffness: 500, damping: 100 });
   const mouseY = useSpring(0, { stiffness: 500, damping: 100 });
 
@@ -23,21 +23,22 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div
       onMouseMove={onMouseMove}
-      className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/25 border-zinc-700 lg:border-zinc-900 "
+      className={`overflow-hidden relative duration-700 border rounded-xl hover:bg-lime-800/5 group md:gap-8 hover:border-lime-600/30 border-lime-500/25 lg:border-zinc-800`}
     >
       <div className="pointer-events-none">
         <div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
         <motion.div
-          className="absolute inset-0 z-10  bg-gradient-to-br opacity-100  via-zinc-600/50  transition duration-1000 group-hover:opacity-50 "
+          className={`absolute inset-0 z-10  bg-gradient-to-br opacity-0  via-lime-800/70  transition duration-1000 group-hover:opacity-30`}
           style={style}
         />
         <motion.div
-          className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
+          className={`absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100`}
           style={style}
         />
       </div>
-
-      {children}
+      <div className={`text-lime-600 lg:text-zinc-300/50 hover:text-lime-300`}>
+        {children}
+      </div>
     </div>
   );
 };
