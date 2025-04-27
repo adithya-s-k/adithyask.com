285aeed2cceddba596c43bf9bea93248b3ce42a3
adithya-s-k <adithyaskolavi@gmail.com>
2025-04-27 19:30:23 +0530

    added posthog lib - Adithya S K

--- cv/src/app/layout.tsx
+++ cv/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
-import Script from "next/script";
-import { RESUME_DATA } from "@/data/resume-data"; // Import RESUME_DATA
+import { RESUME_DATA } from "@/data/resume-data";
+import { PostHogProvider } from "./providers";
 
 import "./globals.css";
 import React from "react";
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     siteName: RESUME_DATA.name,
     images: [
       {
-        url: "/og-image.png", // Relative path to the image in /public
+        url: "/website_ss.png", // Updated to use website_ss.png
         width: 1200,
         height: 630,
         alt: `${RESUME_DATA.name} - ${RESUME_DATA.about}`,
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
     description: RESUME_DATA.summary,
     // site: "@your_twitter_handle", // Add site handle if available
     creator: "@adithya_s_k", // Already present, keep it
-    images: ["/og-image.png"], // Use the same OG image (relative path)
+    images: ["/website_ss.png"], // Updated to use website_ss.png
   },
   alternates: {
     canonical: RESUME_DATA.personalWebsiteUrl,
@@ -87,21 +87,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <head>
-        {/* PostHog */}
-        <Script
-          id="posthog"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags resetGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-              posthog.init('phc_OL7nUCVeKtVJe8eHSKGs8zPTQAyr0hm8opAPFdFlkBz', { api_host: 'https://us.i.posthog.com', person_profiles: 'identified_only' })
-            `,
-          }}
-        />
-      </head>
-      <body>{children}</body>
-      <Analytics /> {/* Vercel Analytics */}
+      <body>
+        <PostHogProvider>
+          {children}
+          <Analytics /> {/* Vercel Analytics */}
+        </PostHogProvider>
+      </body>
     </html>
   );
 }
